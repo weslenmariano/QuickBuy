a77358717594ace64a293edc8891d35a5f3cefbd
weslenmariano <weslen.mariano@hotmail.com>
2019-11-27 10:40:26 -0300

    criando servico de usuario

--- QuickBuy.Web/ClientApp/src/app/app.module.ts
+++ QuickBuy.Web/ClientApp/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ProdutoComponent } from './produto/produto.component';
 import { LoginComponent } from './usuario/login/login.component';
 import { GuardaRotas } from './autorizacao/guarda.rotas';
+import { UsuarioServico } from './servicos/usuario/usuario.servico';
 
 
 @NgModule({
@@ -37,7 +38,7 @@ import { GuardaRotas } from './autorizacao/guarda.rotas';
 
     ])
   ],
-  providers: [],
+  providers: [UsuarioServico],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ QuickBuy.Web/ClientApp/src/app/servicos/usuario/usuario.servico.ts
@@ -0,0 +1,27 @@
+import { Injectable, Inject } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Usuario } from "../../modelo/usuario";
+
+@Injectable({
+    providedIn:"root"
+})
+export class UsuarioServico {
+
+    private baseURL: string
+    constructor(private http: HttpClient, @Inject('BASE_USRL') baseUrl: string) {
+        this.baseURL = baseUrl;
+    }
+
+    public verificarUsuario(usuario: Usuario): Observable<Usuario> {
+
+        const headers = new HttpHeaders().set('content-type', 'application/json');
+        var body = {
+            email: usuario.email,
+            senha: usuario.senha
+        }
+
+        // this.baseURL = raiz do site que pode ser exemplo.: http://www.quickbuy.com.br
+        return this.http.post<Usuario>(this.baseURL + "api/usuario", body, { headers });
+    }
+}
